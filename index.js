3c2dfb96e5512de58c549ed343e36cd506181bea
SergioCaMi <sergiocami84@gmail.com>
2025-05-30 11:28:11 +0200

    update

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ const validateData = (req, res) => {
   if (!req.body.category || !validCategories.includes(req.body.category)) {
     return res.status(400).json({ error: "Categoría inválida." });
   }
-  return null;
 }
 
 /**
@@ -98,6 +97,7 @@ app.post("/api/expenses", (req, res) => {
   const expenses = readExpenses();
   const { description, amount, category } = req.body;
   validateData(req,res);
+  
   const newExpense = {
     id: getNextId(expenses),
     description,
@@ -132,9 +132,12 @@ app.put("/api/expenses/:id", (req, res) => {
       amount,
     };
     validateData(req,res);
+  try {
     saveExpenses(expenses);
     res.status(200).json(expenses[expenseIndex]);
-  }
+  } catch (err) {
+    res.status(500).json({ error: "Error interno al actualizar el gasto." });
+  }  }
 });
 
 // Iteración 4
@@ -146,8 +149,12 @@ app.delete("/api/expenses/:id", (req, res) => {
     return res.status(404).json({ error: "No encontrado." });
   } else {
     expensesDeletedItem = expenses.filter(e=>+e.id != +id);
+ try {
     saveExpenses(expensesDeletedItem);
-    res.status(200).json({ "success": true });
+    res.status(200).json({ success: true, message: "Gasto eliminado correctamente." });
+  } catch (err) {
+    res.status(500).json({ error: "Error interno al eliminar el gasto." });
+  }
   }
 });
 
